3399e916f9b1f046c7bedae14c4f58a5d7ca8ed6
abdoutam <47285809+abdoutam@users.noreply.github.com>
2019-02-03 00:49:13 +0100

    Add files via upload

---
+++ Password.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package password;
+
+/**
+ *
+ * @author fihakhir kh
+ */
+public class Password {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        connexion C=new connexion();
+        C.setVisible(true);
+    }
+    
+}
